426b03ff78ef2f30ff67f2499dcf646ce31f3e19
Jeferson <jefersongabri@gmail.com>
2023-04-25 16:00:59 -0300

    ativar e desativar o menu hamburguer

--- script/dropdown-menu.js
+++ script/dropdown-menu.js
@@ -5,20 +5,15 @@ function initDropDownMenu(){
 const dropDownMenu = document.querySelector('.dropDownEdit')
 const editarBotoes = document.querySelector('.editarBotoes')
 const menu = document.querySelector('.menu')
-menu.addEventListener('click',menuAtivo)
-dropDownMenu.addEventListener('click', handleClick)
-dropDownMenu.addEventListener('touchstart', handleClick)
+
+  menu.addEventListener('click',menuAtivo)
 
 function menuAtivo(e) {
   editarBotoes.classList.toggle('ativo')
+  menu.classList.toggle('ativo')
   e.stopPropagation()
   outsideClick()
 }
-function handleClick() {
-  editarBotoes.classList.add('ativo')
-  outsideClick()
-}
-
 function outsideClick(e) {
   const html = document.documentElement
 html.addEventListener('click', handleOutsideClick)
@@ -26,7 +21,10 @@ html.addEventListener('click', handleOutsideClick)
     if (!editarBotoes.contains(e.target)) {
       html.removeEventListener('click',handleOutsideClick)
       editarBotoes.classList.remove('ativo')
+      menu.classList.toggle('ativo')
     }
   }
 }
+
+
 
