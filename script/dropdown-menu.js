32620e81133125b9366783899d856770c3365031
Jeferson <jefersongabri@gmail.com>
2023-04-25 10:23:29 -0300

    dropDown menu de edição

---
+++ script/dropdown-menu.js
@@ -0,0 +1,31 @@
+function initDropDownMenu(){
+
+}
+
+const dropDownMenu = document.querySelector('.dropDownEdit')
+const editarBotoes = document.querySelector('.editarBotoes')
+const menu = document.querySelector('.menu')
+menu.addEventListener('click',menuAtivo)
+dropDownMenu.addEventListener('click', handleClick)
+dropDownMenu.addEventListener('touchstart', handleClick)
+
+function menuAtivo(e) {
+  editarBotoes.classList.toggle('ativo')
+  e.stopPropagation()
+  outsideClick()
+}
+function handleClick() {
+  editarBotoes.classList.add('ativo')
+  outsideClick()
+}
+
+function outsideClick(e) {
+  const html = document.documentElement
+html.addEventListener('click', handleOutsideClick)
+  function handleOutsideClick(e) {
+    if (!editarBotoes.contains(e.target)) {
+     return editarBotoes.classList.remove('ativo')
+    }
+  }
+}
+
