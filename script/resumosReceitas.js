1a10a71be31de4d25a92a1f3d60f600a4052df64
Jeferson <jefersongabri@gmail.com>
2023-05-14 08:49:30 -0300

    arrumei o codigo dos graficos de resumos de despesa e receita

--- script/resumosReceitas.js
+++ script/resumosReceitas.js
@@ -6,7 +6,6 @@ const divs = divResumos.querySelectorAll('.informacoesDaCategoria')
 const usuarioAtiv = JSON.parse(localStorage.usuarioAtivo)
 const infos = JSON.parse(localStorage.getItem(`informacoes_id${usuarioAtiv.ID}`))
 
-
 const arrayResumoReceitas = infos[7] ? infos[7] : []
 
 let valoresTotais = []
@@ -46,23 +45,27 @@ function ValoresFiltradosPorDias(dias) {
       }
     })
     let valoresSomados = valores.reduce((acomulador, valoresSomados) => +acomulador + valoresSomados, 0,)
-    arrayValoresColetados.push(valoresSomados)
+    arrayValoresColetados.push([valoresSomados, categoria])
   })
 }
 
 // setar os valores
 function setarValores() {
-  arrayValoresColetados.sort((a, b) => b - a)
+  arrayValoresColetados.sort((a, b) => b[0] - a[0])
   arrayValoresColetados.length = 4
+
   divs.forEach((div, n) => {
 
     const valorDiv = div.querySelector('.valorTotalDaCategoria')
     const graficoVerde = div.querySelector('.graficoPorcentagem')
     const porcentagemNumerica = div.querySelector('.porcentagemNumero')
+    const nomeDaCategoria = div.querySelector('.nomeDaCategoria')
 
     let valoresSomados = valoresTotais.reduce((acomulador, valoresSomados) => +acomulador + valoresSomados, 0,)
-    const porcentagem = ((arrayValoresColetados[n] / valoresSomados) * 100).toFixed(0)
-    valorDiv.innerText = arrayValoresColetados[n]
+    const porcentagem = ((arrayValoresColetados[n][0] / valoresSomados) * 100).toFixed(0)
+
+    nomeDaCategoria.innerText = arrayValoresColetados[n][1]
+    valorDiv.innerText = arrayValoresColetados[n][0]
     graficoVerde.style.width = `${porcentagem}%`
     porcentagemNumerica.innerText = `${porcentagem}%`
   })
@@ -70,53 +73,6 @@ function setarValores() {
   valoresTotais = []
   escondeDivZerada()
 
-}
-
-// inserir o nome da categoria na div]
-
-
-function inserirCategoriaNaDiv(dias) {
-  let valores = []
-  let numero = 0
-
-  arrayResumoReceitas.forEach((categoria) => {
-    const Data = new Date()
-    Data.setDate(Data.getDate() - dias)
-    const diasInseridos = Data.toISOString().slice(0, 10)
-    receitasLabel.forEach((receita) => {
-      const dataDaReceita = receita.querySelector('#data').innerText
-      const comparador = receita.querySelector('#categoria').innerText
-      const valor = +receita.querySelector('#valor').innerText.replace('+R$ ', '')
-
-
-      if (comparador === categoria && dataDaReceita > diasInseridos) {
-        valores.push(valor)
-      }
-    })
-
-    let valoresTotais = valores.reduce((acomulador, valoresTotais) => +acomulador + valoresTotais, 0,)
-
-    if (valoresTotais !== 0) {
-
-      const nomePraAlterar = graficoResumoReceita.querySelectorAll('.nomeDaCategoria')
-
-      if (nomePraAlterar[numero]) {
-        nomePraAlterar[numero].innerText = categoria
-        numero++
-      }
-    }
-
-    valores = []
-  })
-  // if (categorias.length) {
-  //   let categoriasLimpa = [...new Set(categorias)];
-  //   categoriasLimpa.forEach((categoria, n) => {
-  //     const nome = graficoResumoReceita.querySelectorAll('.nomeDaCategoria')
-  //     if (nome[n]) {
-  //       nome[n].innerText = categoria
-  //     }
-  //   })
-  // }
 }
 
 // esconde div zerada
@@ -137,7 +93,7 @@ function escondeDivZerada() {
 coletarValores(30)
 ValoresFiltradosPorDias(30)
 setarValores()
-inserirCategoriaNaDiv(30)
+
 
 // ao Click mudar as informacoes na tela por dias
 function atualizaH3(dias) {
@@ -182,14 +138,12 @@ graficoResumoReceita.addEventListener('click', function (e) {
     coletarValores(7)
     ValoresFiltradosPorDias(7)
     setarValores()
-    inserirCategoriaNaDiv(7)
 
   } else if (botaoClicado.innerText === '30 dias') {
     atualizaH3(30)
     coletarValores(30)
     ValoresFiltradosPorDias(30)
     setarValores()
-    inserirCategoriaNaDiv(30)
 
 
   } else if (botaoClicado.innerText === '90 dias') {
@@ -197,10 +151,5 @@ graficoResumoReceita.addEventListener('click', function (e) {
     coletarValores(90)
     ValoresFiltradosPorDias(90)
     setarValores()
-    inserirCategoriaNaDiv(90)
-
   }
 })
-
-
-
