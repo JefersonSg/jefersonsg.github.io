7bd2384e6c62f7a6a7fa4c819d60f3cde297484f
Jeferson <jefersongabri@gmail.com>
2023-05-15 17:16:39 -0300

    transição suave

--- script/slide.js
+++ script/slide.js
@@ -7,6 +7,10 @@ class Slide {
     }
   }
 
+  transiton(active){
+    this.slide.style.transition = active ? 'transform .3s' : ''
+  }
+
   moveSlide(distX){
     this.dist.movePosition = distX
     this.slide.style.transform = `translate3d(${distX}px,0,0)`
@@ -24,6 +28,7 @@ class Slide {
     }
 
     this.wrapper.addEventListener(movetype, this.onMove)
+    this.transiton(false)
   }
 
   updatePosition(clientX) {
@@ -44,7 +49,20 @@ class Slide {
     let movetype = event.type === 'mouseup' ? 'mousemove' : 'touchmove'
     this.wrapper.removeEventListener(movetype, this.onMove)
     this.dist.finalPosition = this.dist.movePosition;
+    this.transiton(true)
+    this.changeSlideOnEnd()
   }
+
+  changeSlideOnEnd(){
+    if (this.dist.movement) {
+      if (this.dist.movement > 50 && this.index.next !== undefined) {
+        this.activeNextSlide()
+      }else if(this.dist.movement < -50 && this.index.prev !== undefined) {
+        this.activePrevSlide()
+      }
+    }
+  }
+
   addSlideEvents(){
     this.wrapper.addEventListener("mousedown", this.onStart)
     this.wrapper.addEventListener("touchstart", this.onStart)
@@ -85,9 +103,21 @@ class Slide {
     this.slideIndexNav(index)
     this.dist.finalPosition = activeSlide.position
   }
+
+  activePrevSlide(){
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev)
+    }
+  }
+  activeNextSlide(){
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next)
+    }
+  }
   
   init() {
     this.bindEvents();
+    this.transiton(true)
     this.addSlideEvents();
     this.slidesConfig()
     return this;
