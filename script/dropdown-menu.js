e638770fdbd753024140f6cc8395ef8df6de9a1c
Jeferson <jefersongabri@gmail.com>
2023-05-24 19:22:19 -0300

    melhorias visuais na pagina de atividades

--- script/dropdown-menu.js
+++ script/dropdown-menu.js
@@ -4,7 +4,6 @@ const menu = document.querySelector('.menu')
   menu.addEventListener('click',menuAtivo)
 
 function menuAtivo(e) {
-  editarBotoes.classList.toggle('ativo')
   menu.classList.toggle('ativo')
   e.stopPropagation()
   outsideClick()
@@ -15,10 +14,8 @@ html.addEventListener('click', handleOutsideClick)
   function handleOutsideClick(e) {
     if (!editarBotoes.contains(e.target)) {
       html.removeEventListener('click',handleOutsideClick)
-      editarBotoes.classList.remove('ativo')
       menu.classList.remove('ativo')
     } else if (editarBotoes.contains(e.target) && e.target.nodeName === 'LI') {
-      editarBotoes.classList.remove('ativo')
       menu.classList.remove('ativo')
     }
   }
